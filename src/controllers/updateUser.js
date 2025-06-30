d8243654083c8ac62009c819f375cbd164a14fe3
Baggio25 <rodrigo.baggio.si@gmail.com>
2025-06-30 14:37:18 -0300

    feat: add helper function to check if id is valid

--- src/controllers/updateUser.js
+++ src/controllers/updateUser.js
@@ -1,9 +1,9 @@
 import { badRequest, ok, serverError } from '../controllers/helpers/http.js';
-import validator from 'validator';
 import { UpdateUserUseCase } from '../use-cases/updateUser.js';
 import { EmailAlreadyInUseError } from '../errors/user.js';
 import {
     checkIfEmailIsValid,
+    checkIfIdIsValid,
     checkIfPasswordIsValid,
     emailAlreadyInUseResponse,
     invalidIdResponse,
@@ -15,7 +15,7 @@ export class UpdateUserController {
         try {
             const userId = httpRequest.params.userId;
 
-            const isIdValid = validator.isUUID(userId);
+            const isIdValid = checkIfIdIsValid(userId);
             if (!isIdValid) {
                 return invalidIdResponse();
             }
