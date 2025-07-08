055c5862369116354f9a0db7ff1e123a4b442970
Baggio25 <rodrigo.baggio.si@gmail.com>
2025-07-07 22:50:06 -0300

    fix: updated PostgresGetUserBalanceRepository to use get_user_balance function

--- src/repositories/postgres/user/getUserBalance.js
+++ src/repositories/postgres/user/getUserBalance.js
@@ -4,17 +4,7 @@ export class PostgresGetUserBalanceRepository {
     async execute(userId) {
         const result = await PostgresHelper.query(
             `
-                select 
-                    sum(case when type = 'EARNING' then amount else 0 end) as earnings,
-                    sum(case when type = 'INVESTMENT' then amount else 0 end) as investments,  
-                    sum(case when type = 'EXPENSE' then amount else 0 end) as expenses,
-                    (
-                        sum(case when type = 'EARNING' then amount else 0 end) + 
-                        sum(case when type = 'INVESTMENT' then amount else 0 end) -   
-                        sum(case when type = 'EXPENSE' then amount else 0 end)
-                    ) as balance
-                from transactions 
-                where  user_id = $1;
+                SELECT * FROM get_user_balance($1)
             `,
             [userId],
         );
