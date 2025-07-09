5eeaf47f721e9eb1c762d7ac44de49c5621d89d1
Baggio25 <rodrigo.baggio.si@gmail.com>
2025-07-09 10:12:52 -0300

    fix: delete console.log user id

--- src/repositories/postgres/user/getUserById.js
+++ src/repositories/postgres/user/getUserById.js
@@ -2,7 +2,6 @@ import { prisma } from '../../../../prisma/prisma.js';
 
 export class PostgresGetUserByIdRepository {
     async execute(userId) {
-        console.log('User id: ', userId);
         return await prisma.user.findUnique({
             where: { id: userId },
         });
