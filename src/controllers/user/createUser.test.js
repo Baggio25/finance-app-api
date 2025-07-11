b8c52b380e81c9e93a2744fe80c7c518913c6d8a
Baggio25 <rodrigo.baggio.si@gmail.com>
2025-07-11 11:11:43 -0300

    test: ensure 400 is returned if last_name is not provided

--- src/controllers/user/createUser.test.js
+++ src/controllers/user/createUser.test.js
@@ -50,4 +50,25 @@ describe('Create User Controller', () => {
         // assert
         expect(result.statusCode).toBe(400);
     });
+
+    it('should return 400 if last_name is not provided', async () => {
+        // arrange
+        const createUserUseCase = new CreateUserUseCaseStub();
+        const createUserController = new CreateUserController(
+            createUserUseCase,
+        );
+        const httpRequest = {
+            body: {
+                first_name: 'Jest',
+                email: 'jest.test@gmail.com',
+                password: '123456',
+            },
+        };
+
+        // act
+        const result = await createUserController.execute(httpRequest);
+
+        // assert
+        expect(result.statusCode).toBe(400);
+    });
 });
