bab1e4bdcde04758fa1a42c58b8450c2be48a39e
Baggio25 <rodrigo.baggio.si@gmail.com>
2025-07-11 11:13:27 -0300

    test: ensure 400 is returned if email is not provided

--- src/controllers/user/createUser.test.js
+++ src/controllers/user/createUser.test.js
@@ -71,4 +71,25 @@ describe('Create User Controller', () => {
         // assert
         expect(result.statusCode).toBe(400);
     });
+
+    it('should return 400 if email is not provided', async () => {
+        // arrange
+        const createUserUseCase = new CreateUserUseCaseStub();
+        const createUserController = new CreateUserController(
+            createUserUseCase,
+        );
+        const httpRequest = {
+            body: {
+                last_name: 'Test',
+                first_name: 'Jest',
+                password: '123456',
+            },
+        };
+
+        // act
+        const result = await createUserController.execute(httpRequest);
+
+        // assert
+        expect(result.statusCode).toBe(400);
+    });
 });
