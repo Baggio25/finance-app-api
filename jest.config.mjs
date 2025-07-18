e8e439b3b82fda49305379e8d8f820caa22d543d
Baggio25 <rodrigo.baggio.si@gmail.com>
2025-07-18 20:03:02 -0300

    chore: add test:coverage script

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('jest').Config} */
 const config = {
     coverageDirectory: 'coverage',
-
     coverageProvider: 'v8',
+    collectCoverageFrom: ['src/**/*.js', '!**/generated/**'],
 };
 
 export default config;
