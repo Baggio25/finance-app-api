cc8f16c046e5eb7351042fb4a45a3bf79fd6be16
Baggio25 <rodrigo.baggio.si@gmail.com>
2025-07-23 22:28:05 -0300

    feat: add port production

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import 'dotenv/config.js';
 
 import { app } from './src/app.js';
 
-app.listen(process.env.SERVER_PORT, () =>
-    console.log(
-        `******** Listening on port ${process.env.SERVER_PORT}  ********`,
-    ),
+const PORT = process.env.SERVER_PORT || 39103;
+
+app.listen(PORT, () =>
+    console.log(`******** Listening on port ${PORT}  ********`),
 );
